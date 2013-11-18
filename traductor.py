b174c5e13edbbfcbb3b403382323e09e54d6ad0d
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2013-11-18 13:20:56 -0300

    now proccess standar input

--- traductor.py
+++ traductor.py
@@ -2,8 +2,16 @@
 #-*- coding: UTF-8 -*-
 
 from litebrowser import Browser
+from fileinput import input
+import urllib
 
+BASE_URL = ("http://translate.google.com/translate_a/t?"
+    "client=t&sl=auto&tl=es&hl=es-419&sc=2&ie=UTF-8&oe=UTF-8&"
+    "uptl=es&alttl=en&oc=1&otf=2&ssel=0&tsel=0&q=%s")
 b = Browser()
 b.go("http://translate.google.com")
-b.go("http://translate.google.com/translate_a/t?client=t&sl=auto&tl=es&hl=es-419&sc=2&ie=UTF-8&oe=UTF-8&uptl=es&alttl=en&oc=1&otf=2&ssel=0&tsel=0&q=hi,%20how%20are%20you?")
-print b.get_html()
+
+for line in input():
+    text = urllib.quote(line)
+    b.go(BASE_URL % text)
+    print b.get_html()
